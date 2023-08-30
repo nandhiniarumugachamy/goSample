be722e4762d5387e541ad0595a6086a53bd22e1e
Nandhini Arumugachamy <nandhiniarumugachamy@Nandhinis-MacBook-Pro.local>
2023-08-30 12:54:36 +0530

    changes for v2

--- packagelevel.go
+++ packagelevel.go
@@ -1,3 +1,10 @@
 package main
 
 var packVar = "sample"
+
+func sample() string {
+	return "my sample from packagelevel.go in module nandhini.com/mymodule"
+}
+func fromv2() string {
+	return "v2 version"
+}
